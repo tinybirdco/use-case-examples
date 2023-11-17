8bab7e3b701f6249ec20b821e086a04d59f696ff
Elena Torró <elena@tinybird.co>
2023-11-17 12:53:12 +0100

    Test waiting a bit if it merges it by itself

--- change_ttl/deploy/0.0.1/ci-deploy.sh
+++ change_ttl/deploy/0.0.1/ci-deploy.sh
@@ -3,3 +3,8 @@ set -e
 
 tb datasource append analytics_events datasources/fixtures/analytics_events.ndjson
 tb deploy --yes --populate --fixtures --wait
+
+WAIT_SECONDS_FOR_MERGE=5
+echo "Waiting for $WAIT_SECONDS_FOR_MERGE seconds..."
+sleep "$WAIT_SECONDS_FOR_MERGE"
+echo "Done waiting."
