60561998999d8fd9d81052cdd008588638aec91e
Juan Madurga <jlmadurga@gmail.com>
2023-11-20 17:48:35 +0100

    populate with a mv

--- change_copy_pipe/deploy/0.0.1/cd-deploy.sh
+++ change_copy_pipe/deploy/0.0.1/cd-deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-tb deploy
-tb pipe copy run copy_bots_snapshot_old_to_v1 --wait --yes
+tb deploy --populate --wait
--- change_copy_pipe/deploy/0.0.1/ci-deploy.sh
+++ change_copy_pipe/deploy/0.0.1/ci-deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-tb deploy --fixtures --wait
-tb pipe copy run copy_bots_snapshot_old_to_v1 --wait --yes
+tb deploy --populate --fixtures --wait
