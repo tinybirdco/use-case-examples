91222c7c650d9f01e239031063196d5c742a073a
alrocar <alrocar@tinybird.co>
2023-12-05 23:46:13 +0100

    add column to bigquery datasource

--- v3/add_column_BQ_ds/deploy/0.0.3/cd-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-tb release create --semver ${VERSION}
-tb --semver ${VERSION} deploy --yes
-tb release preview --semver ${VERSION}
-tb release promote --semver ${VERSION}
-tb release ls
