d377382eb4f75ab41824043b74c6d73a09458933
Alberto Juan <alberto@tinybird.co>
2023-12-14 12:01:16 +0100

    Promote the release

--- v3/add_column_materialized_view/deploy/0.0.1/ci-deploy.sh
+++ v3/add_column_materialized_view/deploy/0.0.1/ci-deploy.sh
@@ -5,4 +5,5 @@ source .tinyenv
 tb release create --semver ${VERSION}
 tb --semver ${VERSION} deploy --fixtures --fork-downstream
 tb release preview --semver ${VERSION}
+tb release promote --semver ${VERSION}
 tb release ls
