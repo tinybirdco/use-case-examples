0c138e2093ce27ed5faeb80fc6d31f9ba2393d0c
alrocar <alrocar@tinybird.co>
2023-12-15 21:33:38 +0100

    add custom deployment

--- v3_delete_simple_resource/deploy/1.0.0/cd-deploy.sh
+++ v3_delete_simple_resource/deploy/1.0.0/cd-deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 
-tb deploy
+tb --semver $VERSION deploy
+tb --semver $VERSION pipe rm kips --yes
+tb --semver $VERSION pipe rm analytics_sessions_mv --yes
+tb --semver $VERSION datasource rm analytics_sessions --yes
--- v3_delete_simple_resource/deploy/1.0.0/ci-deploy.sh
+++ v3_delete_simple_resource/deploy/1.0.0/ci-deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
-tb deploy --populate --fixtures --wait
+tb --semver $VERSION deploy --populate --fixtures --wait
+tb --semver $VERSION pipe rm kips --yes
+tb --semver $VERSION pipe rm analytics_sessions_mv --yes
+tb --semver $VERSION datasource rm analytics_sessions --yes
+tb release promote --semver $VERSION
