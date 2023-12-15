cc7f66fcbd1588e9f839ae17c6da8275f094930c
alrocar <alrocar@tinybird.co>
2023-12-15 21:36:36 +0100

    fix

--- v3_delete_simple_resource/deploy/1.0.0/cd-deploy.sh
+++ v3_delete_simple_resource/deploy/1.0.0/cd-deploy.sh
@@ -2,6 +2,6 @@
 set -e
 
 tb --semver $VERSION deploy
-tb --semver $VERSION pipe rm kips --yes
+tb --semver $VERSION pipe rm kpis --yes
 tb --semver $VERSION pipe rm analytics_sessions_mv --yes
 tb --semver $VERSION datasource rm analytics_sessions --yes
--- v3_delete_simple_resource/deploy/1.0.0/ci-deploy.sh
+++ v3_delete_simple_resource/deploy/1.0.0/ci-deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 tb --semver $VERSION deploy --populate --fixtures --wait
-tb --semver $VERSION pipe rm kips --yes
+tb --semver $VERSION pipe rm kpis --yes
 tb --semver $VERSION pipe rm analytics_sessions_mv --yes
 tb --semver $VERSION datasource rm analytics_sessions --yes
 tb release promote --semver $VERSION
