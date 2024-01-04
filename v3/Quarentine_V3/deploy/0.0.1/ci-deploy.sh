e32740e845999a5c901a76a56e5783414c67de76
Alberto Juan <alberto@tinybird.co>
2024-01-04 09:53:58 +0100

    Load fixtures in ci

--- v3/Quarentine_V3/deploy/0.0.1/ci-deploy.sh
+++ v3/Quarentine_V3/deploy/0.0.1/ci-deploy.sh
@@ -6,6 +6,6 @@ tb datasource append analytics_events datasources/fixtures/analytics_events_erro
 
 set -e
 
-tb --semver 0.0.1 deploy --v3
+tb --semver 0.0.1 deploy --fixtures --v3
 tb --semver 0.0.1 pipe copy run analytics_events_quarantine_to_final --wait --yes
 
