e8cb6503ef615458cae426a4c8d7104a000db212
Alberto Juan <alberto@tinybird.co>
2024-01-05 12:15:01 +0100

    Append wrong data after deploy

--- v3/Quarentine_V3/deploy/0.0.1/ci-deploy.sh
+++ v3/Quarentine_V3/deploy/0.0.1/ci-deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-set +e
-
-# This line is only for demo purposes, It's adding wrong data to the analytics_events datasource
-tb datasource append analytics_events datasources/fixtures/analytics_events_errors.ndjson
-
-set -e
-
 tb deploy --fixtures --v3
 
-sleep 30
+set +e
+tb datasource append analytics_events datasources/fixtures/analytics_events_errors.ndjson
+set -e
 
 tb pipe copy run analytics_events_quarantine_to_final --wait --yes
 
