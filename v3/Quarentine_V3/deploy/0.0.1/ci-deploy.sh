8a715ccf6ef7d8c6cdeb1477889f13a67d7ff5a5
Alberto Juan <alberto@tinybird.co>
2024-01-05 12:30:15 +0100

    Try without v3 flag

--- v3/Quarentine_V3/deploy/0.0.1/ci-deploy.sh
+++ v3/Quarentine_V3/deploy/0.0.1/ci-deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-tb deploy --fixtures --v3
-
 set +e
+
 tb datasource append analytics_events datasources/fixtures/analytics_events_errors.ndjson
+
 set -e
+
+tb deploy --fixtures
 
 tb pipe copy run analytics_events_quarantine_to_final --wait --yes
 
