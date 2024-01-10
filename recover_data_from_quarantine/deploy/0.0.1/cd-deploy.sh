4e530d8fc7293f73e0fd23c5748930d6d9e6bc05
Javier Álvarez Medina <xavijam@gmail.com>
2024-01-10 11:30:05 +0100

    Removed old quarantine job use case

--- recover_data_from_quarantine/deploy/0.0.1/cd-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-tb deploy
-tb pipe copy run analytics_events_quarantine_to_final --wait --yes
--- recover_data_from_quarantine/deploy/0.0.1/ci-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-tb datasource append analytics_events datasources/fixtures/analytics_events_errors.ndjson
-tb deploy --fixtures
-tb pipe copy run analytics_events_quarantine_to_final --wait --yes
-# wait 10 seconds so the data quality tests over `datasources_ops_log` have the data available
-sleep 10
