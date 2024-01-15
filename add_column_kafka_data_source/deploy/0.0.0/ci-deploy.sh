518fca3bd773fda822231cc728fd0f5df98ee9f3
Alberto Juan <alberto@tinybird.co>
2024-01-15 16:01:27 +0100

    --populate is required to skip the quaratines doesn't exist error

--- add_column_kafka_data_source/deploy/0.0.0/ci-deploy.sh
+++ add_column_kafka_data_source/deploy/0.0.0/ci-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-tb --semver 0.0.0 deploy --v3 --yes # Hack to avoid --fixtures, which has a known bug being fixed
+
+tb --semver 0.0.0 deploy --populate --v3 --yes # Hack to avoid --fixtures, which has a known bug being fixed
