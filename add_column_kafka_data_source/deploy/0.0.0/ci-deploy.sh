dd52139379594b1ae3ce35ffd276c27ba0e438d3
Alberto Juan <alberto@tinybird.co>
2024-01-15 16:08:13 +0100

    More info custom deployment

--- add_column_kafka_data_source/deploy/0.0.0/ci-deploy.sh
+++ add_column_kafka_data_source/deploy/0.0.0/ci-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-
+echo "tb --semver 0.0.0 deploy --populate --v3 --yes"
 tb --semver 0.0.0 deploy --populate --v3 --yes # Hack to avoid --fixtures, which has a known bug being fixed
