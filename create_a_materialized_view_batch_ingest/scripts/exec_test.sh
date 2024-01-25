0cdf82e4ffeb8ac29dff80af831250a750636d3f
gnzjgo <gonzalojgo@gmail.com>
2024-01-25 14:20:04 +0100

    update exec, add release action

--- create_a_materialized_view_batch_ingest/scripts/exec_test.sh
+++ create_a_materialized_view_batch_ingest/scripts/exec_test.sh
@@ -1,11 +1,18 @@
-
 #!/usr/bin/env bash
 
 export TB_VERSION_WARNING=0
+export VERSION=$1
 
 run_test() {
     t=$1
     echo "** Running $t **"
+    # Check if VERSION is provided
+    if [[ -n $VERSION ]]; then
+        echo "VERSION found: $VERSION"
+        sed -i "s/tb/tb --semver $VERSION/" $t
+    else
+        echo "VERSION not found"
+    fi
     echo "** $(cat $t)"
     if res=$(bash $t $2 | diff -B ${t}.result -); then
         echo 'OK';
@@ -19,7 +26,7 @@ run_test() {
 export -f run_test
 
 fail=0
-parallel -j 4 --halt soon,fail=1 run_test {} $1 ::: $(find ./tests -name "*.test") || fail=1
+find ./tests -name "*.test" -print0 | xargs -0 -I {} -P 4 bash -c 'run_test "$@"' _ {} $VERSION || fail=1
 
 if [ $fail == 1 ]; then
   exit -1;
