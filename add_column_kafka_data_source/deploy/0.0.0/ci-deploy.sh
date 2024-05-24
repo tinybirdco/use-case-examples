a65ec31cbba8cfd70a3d909557ceae3acdf5f5be
alrocar <alrocar@tinybird.co>
2024-05-24 14:20:28 +0200

    restart

--- add_column_kafka_data_source/deploy/0.0.0/ci-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set +e # Allow errors, the append command will fail
-tb datasource append my_kafka_ds datasources/fixtures/my_kafka_ds.ndjson # Hack, it's required to create quarantine table
-set -e # Deployment should not fail
-
-tb --semver 0.0.0 deploy --v3 --yes
